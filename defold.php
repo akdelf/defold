3fcd11e3ddc9bda8cfe578c486be419521cd8daf
AK Delfin <ak@argumenti.ru>
2015-03-26 14:50:25 +0300

    add defold.php

---
+++ defold.php
@@ -0,0 +1,15 @@
+<?
+	
+	//folders
+	define ('SITEPATH', __DIR__.DIRECTORY_SEPARATOR); //sitepath
+	
+	define ('APP', SITEPATH.'/app');
+
+	define('PUB', __DIR__.DIRECTORY_SEPARATOR);
+	define('IMGPATH', PUB.DIRECTORY_SEPARATOR.);
+	define('IMGCACHE', IMGCACHE.DIRECTORY_SEPARATOR.'preview/');
+	define('VIEW', PUB.'views');
+
+
+
+	
