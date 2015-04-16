a75530ff9ac3b90ce0800535794db062ee555cd7
AK Delfin <ak.delf@gmail.con>
2015-04-17 01:05:05 +0300

    add find app and pub path

--- defold.php
+++ defold.php
@@ -3,10 +3,10 @@
 	function defold($dfilter = '', $fstart = '') {
 
 		
-		if (isset($_SERVER['SERVER_NAME']){
+		if (isset($_SERVER['SERVER_NAME'])){
 		
-			if (isset($_SERVER['REQUEST_SCHEME'])
-				$def_method = $_SERVER['REQUEST_SCHEME']
+			if (isset($_SERVER['REQUEST_SCHEME']))
+				$def_method = $_SERVER['REQUEST_SCHEME'];
 			else
 				$def_method = 'http';	
 	
@@ -15,17 +15,38 @@
 		
 
 		//maindir
-
-		if ($pos = strrpos(__DIR__, 'vendor'){
+		if ($pos = strrpos(__DIR__, 'vendor')){
 			$dir = substr(__DIR__, 0, $pos);
 		}
-		elseif (isset($_SERVER['DOCUMENT_ROOT']){
+		elseif (isset($_SERVER['DOCUMENT_ROOT'])){
 			$dir = $_SERVER['DOCUMENT_ROOT'];
 		}
 		else
 		 	$dir = __DIR__;	
 
-		define('SITEPATH', $dir);
+		
+		/**
+		*search application path
+		*/
+		if (is_dir(SITEPATH.'app'))
+			define('APP', SITEPATH.'app'.'/');	
+		elseif (is_dir(SITEPATH.'application'))
+			define('APP', SITEPATH.'application'.'/');
+		
+
+		/**
+		* search static path
+		*/
+		if (is_dir(SITEPATH.'pub'))
+			define('PUB', SITEPATH.'pub'.'/');
+		elseif (is_dir(SITEPATH.'static'))
+			define('PUB', SITEPATH.'static'.'/');
+
+
+		
+
+
+		/*define('SITEPATH', $dir);
 
 		define('APP', SITEPATH.DIRECTORY_SEPARATOR.'APP'.DIRECTORY_SEPARATOR);
 		define('VIEW', APP.DIRECTORY_SEPARATOR.'views');
@@ -33,7 +54,7 @@
 		define('PUB', __DIR__.DIRECTORY_SEPARATOR);
 		define('IMGPATH', PUB.DIRECTORY_SEPARATOR.);
 		define('IMGCACHE', IMGCACHE.DIRECTORY_SEPARATOR.'preview/');
-		define('VIEW', PUB.'views');
+		define('VIEW', PUB.'views');*/
 
 
 	}
@@ -42,15 +63,8 @@
 	
 
 
-	//folders
-	define ('SITEPATH', __DIR__.DIRECTORY_SEPARATOR); //sitepath
 	
-	define ('APP', SITEPATH.'/app');
-
-	define('PUB', __DIR__.DIRECTORY_SEPARATOR);
-	define('IMGPATH', PUB.DIRECTORY_SEPARATOR.);
-	define('IMGCACHE', IMGCACHE.DIRECTORY_SEPARATOR.'preview/');
-	define('VIEW', PUB.'views');
+	
 
 
 
